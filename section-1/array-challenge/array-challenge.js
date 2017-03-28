2891695f848aaf15ac8808e11f3ec3985c771fad
bgallagher-9 <b.gallagher9@gmail.com>
2017-03-28 16:21:21 -0500

    more JS labs

--- section-1/array-challenge/array-challenge.js
+++ section-1/array-challenge/array-challenge.js
@@ -1,11 +1,30 @@
 function arrayReverser(arr) {
+  var output = [];
+  for (var i = arr.length-1; i > -1; i--) {
+    output.push(arr[i]);
+  }
+  return output;
 }
 
+
 function sumArrayOfNumbers(arr) {
+  var i = 0;
+  var sum = 0;
+  for (var i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 }
 
 
 function numbersOver10(arr) {
+  var sum = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > 10) {
+      return arr[i];
+    }
+  }
+  return sum;
 }
 
 function numbersOverX(arr, num) {
